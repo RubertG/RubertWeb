370b90bd556239a089cba25a6ed2040b1959304c
Rubert_G <rubertgonzalez5@gmail.com>
2023-05-01 23:34:32 -0500

    Correccion proyect

--- src/components/pages/project/Project.jsx
+++ src/components/pages/project/Project.jsx
@@ -7,19 +7,27 @@ export function Project({ project }) {
    const [className, setClassName] = useState("container-proyect hidden")
 
    useEffect(() => {
-      const img = new Image()
-      img.src = project.imgs[0]
-      img.onload = () => {
+      if (window.innerWidth > 768) {
          setClassName("container-proyect")
-      };
+      } else {
+         const img = new Image()
+         img.src = project.imgs[0]
+         img.onload = () => {
+            setClassName("container-proyect")
+         };
+      }
    }, [])
 
    return (
       <div className={className}>
-         <img
-            src={project.imgs[0]}
-            alt={`Image of ${project.name}`}
-            loading="lazy" />
+         {
+            window.innerWidth > 768 && (
+               <img
+                  src={project.imgs[0]}
+                  alt={`Image of ${project.name}`}
+                  loading="lazy" />
+            )
+         }
          <h3 className="title-gradient">{project.name}</h3>
          <p>{project.smallDesc}</p>
          <Link to={project.id} className="title-gradient">Ver más<span className="line-title line-title-fine"></span></Link>
